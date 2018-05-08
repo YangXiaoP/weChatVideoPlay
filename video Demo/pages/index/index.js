05697d9bac2ca01f9c20d56b24d2369f95577de2
popperYY <18798800250@163.com>
2018-05-08 19:13:27 +0800

    preject finish

---
+++ video Demo/pages/index/index.js
@@ -0,0 +1,20 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+  
+  },
+  
+  onLoad: function () {
+   
+  },
+  formSubmit: function (e) {
+    var videoId = e.detail.value.video_id;
+    console.log('form发生了submit事件，携带数据为：', e.detail.value)
+    wx.navigateTo({
+      url: '../videoPlay/videoPlay?vid=' + videoId,
+    })
+  }
+})
---
+++ video Demo/pages/videoPlay/videoPlay.js
@@ -0,0 +1,116 @@
+
+var part_urls = {};
+var host;
+var videoPage;
+var pageArr = new Array()
+
+Page({
+  data: {
+    videoUrls: '',
+    token: ''
+  },
+  // 请求视频信息
+  getVideoInfo: function (vid) {
+    var that = this;
+    var videoUrl = 'https://vv.video.qq.com/getinfo?otype=json&appver=3.2.19.333&platform=11&defnpayver=1&vid=' + vid;
+    wx.request({
+      url: videoUrl, 
+      success: function (res) {
+        var dataJson = res.data.replace(/QZOutputJson=/, '') + "qwe";
+        var dataJson1 = dataJson.replace(/;qwe/, '');
+        var data = JSON.parse(dataJson1);
+        var fn_pre = data.vl.vi[0].lnk
+        host = data['vl']['vi'][0]['ul']['ui'][0]['url']
+        var streams = data['fl']['fi']
+        var seg_cnt = data['vl']['vi'][0]['cl']['fc']
+        if (parseInt(seg_cnt) == 0) {
+          seg_cnt = 1
+        }
+        var best_quality = streams[streams.length - 1]['name']
+        var part_format_id = streams[streams.length - 1]['id']
+
+        for (var i = 1; i < (seg_cnt + 1); i++) {
+          var filename = fn_pre + '.p' + (part_format_id % 10000) + '.' + i + '.mp4';
+          console.log(filename);
+          pageArr.push(i);
+          that.requestVideoUrls(part_format_id, vid, filename, 'index' + i);
+
+        }
+
+      }
+    })
+  },
+  // 解析视频正真的地址
+  requestVideoUrls: function (part_format_id, vid, fileName, index) {
+    var keyApi = "https://vv.video.qq.com/getkey?otype=json&platform=11&format=" + part_format_id + "&vid=" + vid + "&filename=" + fileName + "&appver=3.2.19.333"
+    var that = this;
+    wx.request({
+      url: keyApi,
+      success: function (res) {
+        var dataJson = res.data.replace(/QZOutputJson=/, '') + "qwe";
+        var dataJson1 = dataJson.replace(/;qwe/, '');
+        var data = JSON.parse(dataJson1);
+        if (data.key != undefined) {
+          var vkey = data['key']
+          var url = host + fileName + '?vkey=' + vkey;
+          part_urls[index] = String(url)
+          that.setData({
+            videUrl: part_urls.index1
+          });
+        }
+      }
+    })
+  },
+
+  onLoad: function (options) {
+   
+    if (options.vid != undefined) {
+      this.setData({
+        file_id: options.vid
+      });
+    } else {
+  wx.showToast({
+    title: '未传入视频id',
+  })
+    }
+
+
+    videoPage = 1;
+    pageArr = new Array();
+    part_urls = {};
+    this.getVideoInfo(this.data.file_id);
+  },
+  // 因为视频超过10分钟之后，会分段，所以当视频为多段的时候，
+  // 自动播放下一段视频
+  playEnd: function () {
+
+    if (videoPage > parseInt(pageArr.length)) {
+      // part_urls = {};
+      videoPage = 1;
+      this.videoContext.exitFullScreen
+    } else {
+      videoPage++;
+      var index = 'index' + videoPage;
+      this.setData({
+        videUrl: ''
+      });
+      this.setData({
+        videUrl: part_urls[index]
+      });
+
+    }
+  },
+  onReady: function () {
+    // 页面渲染完成
+    this.videoContext = wx.createVideoContext('myVideo')
+  },
+  onShow: function () {
+    // 页面显示
+  },
+  onHide: function () {
+    // 页面隐藏
+  },
+  onUnload: function () {
+    // 页面关闭
+  }
+})
