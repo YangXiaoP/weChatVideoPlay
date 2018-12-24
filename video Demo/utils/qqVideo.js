74a86731c8d597f8fb9c07e9e237577ed346c3f0
popperYY <18798800250@163.com>
2018-12-24 16:43:59 +0800

    1.修复网页链接前缀带有cover字段的视频链接不能播放的问题。2.增加了播放错误的提示

--- video Demo/utils/qqVideo.js
+++ video Demo/utils/qqVideo.js
@@ -9,7 +9,9 @@ getVideoes (vid) {
   count = 0;
   videoSrcArr = new Array();
   var that = this;
-    var videoUrl = 'https://vv.video.qq.com/getinfo?otype=json&appver=3.2.19.333&platform=11&defnpayver=1&vid=' + vid;
+  var platforms = [4100201, 11]
+  for (const platform in platforms) {
+    var videoUrl = 'https://vv.video.qq.com/getinfo?otype=json&appver=3.2.19.333&platform='+platform+'&defnpayver=1&vid=' + vid;
     var host;
     return new Promise(function (resolve) {
     wx.request({
@@ -18,26 +20,55 @@ getVideoes (vid) {
         var dataJson = res.data.replace(/QZOutputJson=/, '') + "qwe";
         var dataJson1 = dataJson.replace(/;qwe/, '');
         var data = JSON.parse(dataJson1);
+        if (data.msg == 'cannot play outside') {
+          wx.showToast({
+            title: 'cannot play outside',
+            icon: 'none',
+            duration: 2000
+          })
+          return nil;
+        }
+        if (data.msg == 'vid status wrong') {
+          wx.showToast({
+            title: 'vid status wrong',
+            icon: 'none',
+            duration: 2000
+          })
+          return nil;
+        }
         var fn_pre = data.vl.vi[0].lnk
         host = data['vl']['vi'][0]['ul']['ui'][0]['url']
         var streams = data['fl']['fi']
         var seg_cnt = data['vl']['vi'][0]['cl']['fc']
+        var filename = data['vl']['vi'][0]['fn'];
+        var fc_cnt  = seg_cnt
+        var video_type
+        var magic_str
         if (parseInt(seg_cnt) == 0) {
           seg_cnt = 1
+        }else {
+          fn_pre, magic_str, video_type = filename.split('.')
         }
-        var best_quality = streams[streams.length - 1]['name']
-        var part_format_id = streams[streams.length - 1]['id']
-      console.log(seg_cnt);
-         for (var i = 1; i < (seg_cnt + 1); i++) {
-           var filename = fn_pre + '.p' + (part_format_id % 10000) + '.' + i + '.mp4';
-           requestVideoUrls(part_format_id, vid, filename, host, seg_cnt).then(function (response) {
+
+        var part_urls = new Array()
+        var total_size = 0
+        var part_format_id
+        for (var i = 1; i < (seg_cnt + 1); i++) {
+          if (fc_cnt == 0) {
+            var part_format_ids = data['vl']['vi'][0]['cl']['keyid'].split('.')
+            part_format_id = part_format_ids[part_format_ids.length-1]
+          }else {
+            part_format_id = data['vl']['vi'][0]['cl']['ci'][i - 1]['keyid'].split('.')[1]
+            filename = [fn_pre, magic_str, str(i), video_type].join('.')
+          }
+          requestVideoUrls(part_format_id, vid, filename, host, seg_cnt).then(function (response) {
         resolve(response);
-           });
+         });
         }
-      
       }
     })
     })
+  }
   },
 // 解析视频真正的地址
 };
